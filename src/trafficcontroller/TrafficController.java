9538a3ba74383ee4585e384878c599c6923e64f4
hkillam <heather.killam.kw@gmail.com>
2016-09-14 12:08:00 -0400

    New database tables to track populations, how many trials in each one, and which generation they are.

--- src/trafficcontroller/TrafficController.java
+++ src/trafficcontroller/TrafficController.java
@@ -12,7 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
-import javafx.scene.text.*;
+//import javafx.scene.text.*;
 import javafx.geometry.*;
 
 import org.nlogo.app.App;
@@ -22,7 +22,7 @@ import org.nlogo.app.App;
  * @author drago
  */
 public class TrafficController extends Application {
-
+private TextField txtPoplabel;
     public static void main(String[] args) {
         App.main(args);  // launch netlogo
         launch(args);
@@ -32,8 +32,6 @@ public class TrafficController extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        //DataConnection mydb = new DataConnection();
-        //mydb.test();
         // load our NetLogo application
         try {
             java.awt.EventQueue.invokeAndWait(
@@ -59,7 +57,9 @@ public class TrafficController extends Application {
         root.setHgap(5);
         root.setVgap(5);
         root.setPadding(new Insets(25, 25, 25, 25));
+ //       root.setGridLinesVisible(true);
 
+        // original test button - not being added.
         Button btnSetup = new Button();
         btnSetup.setPrefWidth(200);
         btnSetup.setText("Setup and Run Netlogo");
@@ -78,87 +78,111 @@ public class TrafficController extends Application {
                 }
             }
         });
-
         //root.add(btnSetup, 1, 1);
+
         Label lblResultsFilename = new Label();
         lblResultsFilename.setText("Building List Results");
-        root.add(lblResultsFilename, 1, 7);
+        root.add(lblResultsFilename, 0, 7);
 
+        /* filename for building results file */
         TextField txtResultsFilename = new TextField();
         txtResultsFilename.setText("C:\\WorksiteTrafficResults\\results-buildings.csv");
         txtResultsFilename.setId("txtResultsFile"); // NOI18N
-        txtResultsFilename.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-            }
-        });
         root.add(txtResultsFilename, 2, 7);
 
         Label lblWorkerResultsFilename = new Label();
         lblWorkerResultsFilename.setText("Performance Results");
-        root.add(lblWorkerResultsFilename, 1, 8);
+        root.add(lblWorkerResultsFilename, 0,8);
+        
         TextField txtWorkerResultsFilename = new TextField();
         txtWorkerResultsFilename.setText("C:\\WorksiteTrafficResults\\results-groupstats.csv");
         txtWorkerResultsFilename.setId("txtResultsFile"); // NOI18N
-        txtWorkerResultsFilename.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-            }
-        });
         root.add(txtWorkerResultsFilename, 2, 8);
 
+        /* text: give the population a name */
+        txtPoplabel = new TextField();
+        txtPoplabel.setText("Give this a name");
+        txtPoplabel.setPrefWidth(200);
+        txtPoplabel.setId("txtPoplabel"); // NOI18N
+        root.add(txtPoplabel, 2, 2);
+
+        /* text: size of population */
         TextField txtIterations = new TextField();
         txtIterations.setText("20");
         txtIterations.setPrefWidth(50);
         txtIterations.setId("txtIterations"); // NOI18N
-        txtIterations.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-            }
-        });
-        root.add(txtIterations, 2, 2);
+        root.add(txtIterations, 4, 2);
+        
         Label lblIterations = new Label();
-        lblIterations.setText(" iterations");
-        root.add(lblIterations, 2, 3);
+        lblIterations.setText("Population size");
+        root.add(lblIterations, 3, 2);
 
         Button btnShuffle = new Button();
-        btnShuffle.setText("Randomize and Run");
+        btnShuffle.setText("Generate Population");
         btnShuffle.setPrefWidth(200);
         btnShuffle.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                int iterations = Integer.parseInt(txtIterations.getText());
-                for (int i = 0; i < iterations; i++) {
-                    try {
-                        Long starttime = System.currentTimeMillis();
-                        App.app().command("set total-ticks 2000");
-                        App.app().command("setup");
-                        App.app().command("make-buildings-dance");
-                        App.app().command("repeat 2000 [ go ]");
-                        App.app().command("write-results");
-                        new CSVReader().ReadWorkerStatsFile("C:\\WorksiteTrafficResults\\results-groupstats.csv", "C:\\WorksiteTrafficResults\\results-buildings.csv", starttime);
-                        // note - slow function.  start time 9:30
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            }
+            @Override public void handle(ActionEvent event) { CreateRandomPopulation(txtIterations);}
         });
-        root.add(btnShuffle, 1, 2);
+        root.add(btnShuffle, 0, 2);
+
+        /* Button - create reproduction population */
+        Button btnReproductions = new Button();
+        btnReproductions.setText("Create reproduction population");
+        btnReproductions.setPrefWidth(200);
+        btnReproductions.setOnAction(new EventHandler<ActionEvent>() {
+            @Override  public void handle(ActionEvent event) { CreateReproductionPopulation();}
+        });
+        root.add(btnReproductions, 0, 3);
 
         // put this scene in the new window, and show it
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setScene(new Scene(root, 450, 300));
         primaryStage.show();
-
-        //new CSVReader().ReadWorkerStatsFile("C:\\WorksiteTrafficResults\\results-groupstats.csv", "C:\\WorksiteTrafficResults\\results-buildings.csv", System.currentTimeMillis());
     }
 
+    // Create a population of building plans.  Each plan places the buildings in
+    // random (successful) locations, avoiding obsticles.  Each plan is then executed
+    // to gather info on how the traffic flows, including total number of trips.
+    // The algorithm for placing buildings was created in NetLogo previously, and is
+    // triggered repeatedly from here.
+    public void CreateRandomPopulation(TextField txtIterations) {
+        // TODO:  show a counter for how many iterations are done.  20 iterations could take more than an hour
+        int iterations = Integer.parseInt(txtIterations.getText());
+        long populationID = new DataConnection().insertRow("population", "(population_type_id, parentpopulationid,label)", "1,0,'" + txtPoplabel.getText() + "'");
+        for (int i = 0; i < iterations; i++) {
+            try {
+                Long starttime = System.currentTimeMillis();
+                App.app().command("set total-ticks 2000");
+                App.app().command("setup");
+                App.app().command("make-buildings-dance");
+                App.app().command("repeat 2000 [ go ]");
+                App.app().command("write-results");
+                new CSVReader().ReadWorkerStatsFile("C:\\WorksiteTrafficResults\\results-groupstats.csv", "C:\\WorksiteTrafficResults\\results-buildings.csv", starttime, populationID);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } 
+    }
+    
+    // Create Reproduction Population
+    // Take the top 80% of the newest 1st gen population, and scramble it.
+    // 
+    public void CreateReproductionPopulation() {
+                
+
+                        // get an existing 1st gen population
+                        // load netlogo startup-world-only
+                        // App.app().command("setup-world-only");
+                        // for every trial
+                        //    scramble and test until we have a valid position for each building.
+                        
+    }
+    
+    // unused.
     public void RunNetlogoStartup() {
         try {
             App.app().command("set total-ticks 2000");
-            //App.app().command("setup");
-            //App.app().command("repeat 2000 [ go ]");
+            App.app().command("setup");
+            App.app().command("repeat 2000 [ go ]");
 
         } catch (Exception ex) {
             ex.printStackTrace();
