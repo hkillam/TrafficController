6706b07b0118af22ba60ced18327ceb2ab1b777d
hkillam <heather.killam.kw@gmail.com>
2016-09-19 10:04:42 -0400

    Report is working.

--- src/trafficcontroller/ResultsTable.java
+++ src/trafficcontroller/ResultsTable.java
@@ -5,14 +5,14 @@
  */
 package trafficcontroller;
 
-import java.sql.ResultSet;
-import javafx.application.Application;
-import javafx.collections.*;
 import javafx.geometry.Insets;
 import javafx.scene.*;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.collections.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.beans.property.SimpleStringProperty;
 
 /**
  *
@@ -20,109 +20,108 @@ import javafx.scene.layout.VBox;
  */
 public class ResultsTable {
 
-    
     public void showResults() {
-//        TableView<PopulationResult> table = new TableView<PopulationResult>();
-        TableView table = new TableView();
+        //TableView<PopulationResult> table = new TableView<PopulationResult>();
         Scene scene = new Scene(new Group());
         Stage stage = new Stage();
         stage.setTitle("Results of every trial");
-        stage.setWidth(600);
+        stage.setWidth(680);
         stage.setHeight(300);
-        ResultSet results = new DataConnection().ShowResults();
- 
-        ObservableList<PopulationResult> data =
-        FXCollections.observableArrayList();
-        
-        try {
-        while (results.next()) {
-            PopulationResult row = new PopulationResult (
-                results.getString(0),
-                results.getString(1),
-                    "","","","",""
-                
-                );
-                data.add(row);
-        }
-        } catch (Exception ex) {}
 
+        ObservableList<PopulationResult> data = new DataConnection().GetResults();
 
- 
+        TableView<PopulationResult> table = new TableView<PopulationResult>(data);
         table.setEditable(true);
- 
-        TableColumn IDCol = new TableColumn("ID");
-        TableColumn typeCol = new TableColumn("Population Type");
-        TableColumn parentCol = new TableColumn("Parent");
-        TableColumn numTrialsCol = new TableColumn("Number of Trials");
-        TableColumn averageTripsCol = new TableColumn("Average # of trips");
-        TableColumn minTripsCol = new TableColumn("Smallest # of trips");
-        TableColumn maxTripsCol = new TableColumn("Greatest # of trips");
-        
-        table.getColumns().addAll(IDCol, typeCol, parentCol, numTrialsCol, averageTripsCol, minTripsCol, maxTripsCol);
-        //table.setItems(data);
+        table.setMinWidth(655);
 
-        
-        
-        
+        TableColumn IDCol = new TableColumn("ID");
+        IDCol.setCellValueFactory(
+                new PropertyValueFactory<PopulationResult, String>("IDCol"));
+
+        TableColumn<PopulationResult, String> typeCol = new TableColumn<>("Population Type");
+        typeCol.setCellValueFactory(new PropertyValueFactory<PopulationResult, String>("typeCol"));
+
+        TableColumn parentCol = new TableColumn("Parent");
+        parentCol.setCellValueFactory(
+                new PropertyValueFactory<PopulationResult, String>("parentpopulationid"));
+
+        TableColumn numTrialsCol = new TableColumn("Number of Trials");
+        numTrialsCol.setMinWidth(20);
+        numTrialsCol.setCellValueFactory(
+                new PropertyValueFactory<PopulationResult, String>("numTrialsCol"));
+
+        TableColumn averageTripsCol = new TableColumn("Average # of trips");
+        averageTripsCol.setCellValueFactory(
+                new PropertyValueFactory<PopulationResult, String>("averageTripsCol"));
+        TableColumn minTripsCol = new TableColumn("Smallest # of trips");
+        minTripsCol.setCellValueFactory(
+                new PropertyValueFactory<PopulationResult, String>("minTripsCol"));
+        TableColumn maxTripsCol = new TableColumn("Greatest # of trips");
+        maxTripsCol.setCellValueFactory(
+                new PropertyValueFactory<PopulationResult, String>("maxTripsCol"));
+
+        table.getColumns().addAll(IDCol, typeCol, parentCol, numTrialsCol, averageTripsCol, minTripsCol, maxTripsCol);
+        table.setItems(data);
+
         final VBox vbox = new VBox();
         vbox.setSpacing(5);
         vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(table);
 
- 
         ((Group) scene.getRoot()).getChildren().addAll(vbox);
- 
+
         stage.setScene(scene);
         stage.show();
-    }    
-    
-    
-    
+    }
+
     public static class PopulationResult {
 
-        private final String IDCol;
-        private final String typeCol;
-        private final String parentCol;
-        private final String numTrialsCol;
-        private final String averageTripsCol;
-        private final String minTripsCol;
-        private final String maxTripsCol;
- 
-        private PopulationResult(String IDCol, String typeCol, String parentCol, String numTrialsCol, String averageTripsCol, String minTripsCol, String maxTripsCol) {
-            this.IDCol = new String(IDCol);
-            this.typeCol = new String(typeCol);
-            this.parentCol = new String(parentCol);
-            this.numTrialsCol = new String(numTrialsCol);
-            this.averageTripsCol = new String(averageTripsCol);
-            this.minTripsCol = new String(minTripsCol);
-            this.maxTripsCol = new String(maxTripsCol);
+        private final SimpleStringProperty IDCol;
+        private final SimpleStringProperty typeCol;
+        private final SimpleStringProperty parentpopulationid;
+        private final SimpleStringProperty numTrialsCol;
+        private final SimpleStringProperty averageTripsCol;
+        private final SimpleStringProperty minTripsCol;
+        private final SimpleStringProperty maxTripsCol;
+
+        public PopulationResult(String IDCol, String typeCol, String parentpopulationid, String numTrialsCol, String averageTripsCol, String minTripsCol, String maxTripsCol) {
+            this.IDCol = new SimpleStringProperty(IDCol);
+            this.typeCol = new SimpleStringProperty(typeCol);
+            this.parentpopulationid = new SimpleStringProperty(parentpopulationid);
+            this.numTrialsCol = new SimpleStringProperty(numTrialsCol);
+            this.averageTripsCol = new SimpleStringProperty(averageTripsCol);
+            this.minTripsCol = new SimpleStringProperty(minTripsCol);
+            this.maxTripsCol = new SimpleStringProperty(maxTripsCol);
         }
- 
+
         public String getIDCol() {
-            return IDCol;
+            return IDCol.get();
         }
- 
-        public String gettypeCol() {
-            return typeCol;
+
+        public String getTypeCol() {
+            return typeCol.get();
         }
- 
-        public String getparentCol() {
-            return parentCol;
+
+        public String getParentpopulationid() {
+            return parentpopulationid.get();
         }
-        public String getnumTrialsCol() {
-            return numTrialsCol;
+
+        public String getNumTrialsCol() {
+            return numTrialsCol.get();
         }
-        public String getaverageTripsCol() {
-            return averageTripsCol;
+
+        public String getAverageTripsCol() {
+            return averageTripsCol.get();
         }
-        public String getminTripsCol() {
-            return minTripsCol;
+
+        public String getMinTripsCol() {
+            return minTripsCol.get();
         }
-        public String getmaxTripsCol() {
-            return maxTripsCol;
+
+        public String getMaxTripsCol() {
+            return maxTripsCol.get();
         }
- 
 
     }
 
-         
 }
